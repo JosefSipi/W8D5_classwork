1fb5342a119f37bb10af239bc070de1c03626460
juliajykim <jkim24@gwu.edu>
2021-04-16 13:55:54 -0400

    Working on _tick method in Clock class

---
+++ Callback/intro_to_callbacks_excercises.js
@@ -0,0 +1,27 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.
+    const date = new Date();
+    this.seconds = date.getSeconds();
+    this.minutes = date.getMinutes();
+    this.hours = date.getHours();
+    setInterval(this._tick(),1000);
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+    console.log(`${this.hours} : ${this.minutes} : ${this.seconds}`);
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+    this.printTime();
+  }
+}
+
+const clock = new Clock();
